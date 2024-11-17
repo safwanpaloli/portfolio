b1cb6558071f2bccca59233f24158b82bec797b6
safwan <safwanpaloli6@gmail.com>
2024-11-18 00:16:26 +0530

    mobie navigation updated

--- components/mobileNav.vue
+++ components/mobileNav.vue
@@ -1,10 +1,10 @@
 <template>
         <div class="w-full md:hidden flex justify-end mr-3">
-            <button @click="useChangeColormode()" class="flex items-center justify-center">
+            <div @click="useChangeColormode()" class="flex items-center justify-center">
                 <div class="flex items-center justify-center text-xl shadow-md w-12 h-12 rounded-full" :class="colorMode.value == 'light' ? 'bg-yellow-300' : 'bg-blue-300'">
                     <Icon :name="colorMode.value == 'light' ? 'material-symbols:sunny-outline-rounded' : 'material-symbols:dark-mode'" size="25px" color="white"/>
                 </div>
-            </button>
+            </div>
          </div>
 </template>
 <script setup lang="ts">
